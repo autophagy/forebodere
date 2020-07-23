7ec9dc00ae7dce58e64f2289d0ca59cae57d067f
Mika Naylor <mail@autophagy.io>
2020-07-23 08:40:32 +0200

    Update discord.py to 1.3.4

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=[
         "Whoosh==2.7.4",
         "wisdomhord==0.3.1",
-        "discord.py==1.3.3",
+        "discord.py==1.3.4",
         "markovify==0.7.1",
         "requests==2.24.0",
     ],
